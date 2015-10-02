78f78ca78196428e803a51f998776e9e4e5a66ae
Robyn Nason <rsn48@cornell.edu>
2015-10-02 16:49:25 -0400

    finished dictset implemenetation and most of the tests. need to figure out how to test the string_of methods thoughgit status!

--- a3-2015fa/myset.ml
+++ a3-2015fa/myset.ml
@@ -254,18 +254,20 @@ struct
   (* returns empty Dictset *)
   let empty = D.empty
 
+  (* Checks if the set s is empty *)
   let is_empty s = s=empty
 
-  (* insert : elt -> set -> set *)
+  (* inserts element k into set s *)
   let insert k s = D.insert s k ()
 
   (* same as insert x empty *)
-  (* let singleton : elt -> set *)
   let singleton k = D.insert empty k ()
 
+  (* returns a new set of the union of sets s1 and s2 *)
   let union s1 s2 =
     D.fold (fun k () d -> insert k d) s1 s2
 
+  (* returns a new set of the intersection of sets s1 and s3 *)
   let intersect s1 s2 =
     D.fold (fun k () d -> if D.member s2 k then insert k d else d) empty s1
 
@@ -293,7 +295,7 @@ struct
 
   (* functions to convert our types to a string. useful for debugging. *)
   let string_of_set s =
-    D.string_of_dict s
+    "Set: "^(D.string_of_dict s)
 
   let string_of_elt k =
     D.string_of_key k
@@ -305,7 +307,24 @@ struct
   (* comprehensive tests to test ALL your functions.              *)
   (****************************************************************)
 
-  (* let test_empty () =  *)
+  (* Test if the empty dict set is an empty dictionary *)
+  let test_empty () =
+    let dict_set_empty = empty in
+    assert(dict_set_empty=D.empty);
+    ()
+
+  (* Test if is_empty will return true for an empty dictset *)
+  let test_is_empty () =
+    let dict_set_empty = empty in
+    assert(is_empty dict_set_empty);
+    ()
+
+  (* Test if is_empty wil return false for a non-empty dictset *)
+  let test_is_empty_false () =
+    let elt = C.gen_random () in
+    let dict_set_not_empty = insert elt empty in
+    assert(not (is_empty dict_set_not_empty));
+    ()
 
   (* Test insertion into empty set *)
   let test_insert () =
@@ -324,10 +343,240 @@ struct
     assert(member new_s1 elt);
     ()
 
+  (* Test singleton set membership *)
+  let test_singleton_elt () =
+    let elt = C.gen_random () in
+    let single_set = singleton elt in
+    assert(not (is_empty single_set));
+    assert(member single_set elt);
+    ()
+
+  (* Test singleton set size = 1 *)
+  let test_singleton_size_1 () =
+    let elt = C.gen_random () in
+    let single_set = singleton elt in
+    assert(not (is_empty single_set));
+    let empty_single_set = remove elt single_set in
+    assert(is_empty empty_single_set);
+    ()
+
+  (* Test union. s1 has 2 elets, s2 has 1. s should contain all 3 *)
+  let test_union_1 () =
+    let elt1 = C.gen_random () in
+    let start_s1 = singleton elt1 in
+    let elt2 = C.gen_random () in
+    let s1 = insert elt2 start_s1 in
+    let elt3 = C.gen_random () in
+    let s2 = singleton elt3 in
+    let s = union s1 s2 in
+    assert(member s elt1);
+    assert(member s elt2);
+    assert(member s elt3);
+    ()
+
+  (* Tests if one set is empty and other is singleton. s is singleton *)
+  let test_union_2 () =
+    let elt1 = C.gen_random () in
+    let s1 = singleton elt1 in
+    let s2 = empty in
+    let s = union s1 s2 in
+    assert(member s elt1);
+    let new_s = remove elt1 s in
+    assert(is_empty new_s);
+    ()
+
+  (* Tests if both sets are empty *)
+  let test_union_3 () =
+    let s1 = empty in
+    let s2 = empty in
+    let s = union s1 s2 in
+    assert(is_empty s);
+    ()
+
+  (* Tests if both sets have the same element if no duplicates *)
+  let test_union_4 () =
+    let elt = C.gen_random () in
+    let start_s1 = singleton elt in
+    let elt2 = C.gen_random () in
+    let s1 = insert elt2 start_s1 in
+    let s2 = singleton elt in
+    let s = union s1 s2 in
+    assert(member s elt);
+    assert(member s elt2);
+    let new_s = (remove elt (remove elt2 s)) in
+    assert(is_empty new_s);
+    ()
+
+  (* Test intersect of two sets with 2 elts each, sharing 1 elt *)
+  let test_intersect_1 () =
+    let elt = C.gen_random () in
+    let elt2 = C.gen_random () in
+    let elt3 = C.gen_random () in
+    let s1 = insert elt (singleton elt2) in
+    let s2 = insert elt (singleton elt3) in
+    let s = intersect s1 s2 in
+    assert(member s elt);
+    assert(not (member s elt2));
+    assert(not (member s elt3));
+    ()
+
+  (* Test intersect of two sets with no shared elts *)
+  let test_intersect_2 () =
+    let s1 = singleton (C.gen_random ()) in
+    let s2 = singleton (C.gen_random ()) in
+    let s = intersect s1 s2 in
+    assert(is_empty s);
+    ()
+
+  (* Test intersect of two sets, one empty *)
+  let test_intersect_3 () =
+    let s1 = singleton (C.gen_random ()) in
+    let s2 = empty in
+    let s = intersect s1 s2 in
+    assert(is_empty s);
+    ()
+
+  (* Test intersect of two empty sets *)
+  let test_intersect_4 () =
+    let s1 = empty in
+    let s2 = empty in
+    let s = intersect s1 s2 in
+    assert(is_empty s);
+    ()
+
+  (* Test removal of a two element set *)
+  let test_remove_1 () =
+    let elt1 = C.gen_random () in
+    let elt2 = C.gen_random () in
+    let s = insert elt1 (singleton elt2) in
+    let new_s = remove elt1 s in
+    assert(member new_s elt2);
+    assert(not(member new_s elt1));
+    ()
+
+  (* Test removal of a one element set *)
+  let test_remove_2 () =
+    let elt = C.gen_random () in
+    let s = singleton elt in
+    let new_s = remove elt s in
+    assert(is_empty new_s);
+    assert(not(member new_s elt));
+    ()
+
+  (* Test removal of element not in set *)
+  let test_remove_3 () =
+    let s = singleton (C.gen_random ()) in
+    let new_s = remove (C.gen_random ()) s in
+    assert(not (is_empty new_s));
+    ()
+
+  (* Test membership true *)
+  let test_member_true () =
+    let elt = C.gen_random () in
+    let s = singleton elt in
+    assert(member s elt);
+    ()
+
+  (* Test membership false *)
+  let test_member_false () =
+    let elt = C.gen_random () in
+    let s = singleton (C.gen_random ()) in
+    assert(not(member s elt));
+    ()
+
+  (* gets the value out of an option *)
+  let test_get_opt opt =
+    match opt with
+    | None -> raise (Invalid_argument "Option.get")
+    | Some (k,s) -> (k,s)
+
+  (* Test choose of two element set *)
+  let test_choose_1 () =
+    let elt = C.gen_random () in
+    let elt1 = C.gen_random () in
+    let s = insert elt1 (singleton elt) in
+    let chosen_opt = choose s in
+    let (chosen_elt, new_s) = test_get_opt chosen_opt in
+    assert(not (member new_s chosen_elt));
+    assert(not (is_empty new_s));
+    ()
+
+  (* Test choose of one element set *)
+  let test_choose_2 () =
+    let elt = C.gen_random () in
+    let s = singleton elt in
+    let chosen_opt = choose s in
+    let (chosen_elt, new_s) = test_get_opt chosen_opt in
+    assert(not (member new_s elt));
+    assert(is_empty new_s);
+    ()
+
+  (* Test choose of zero element set, should raise error *)
+  let test_choose_3 () =
+    let s = empty in
+    let chosen_opt = choose s in
+    try
+      let (chosen_elt, new_s) = test_get_opt chosen_opt in
+      assert(is_empty new_s);
+      ()
+    with
+    | Invalid_argument("Option.get") ->
+    ()
+
+  (* Test fold one 2 element set, replacing both with another element twice *)
+  let test_fold_1 () =
+    let elt = C.gen_random () in
+    let elt1 = C.gen_random () in
+    let elt2 = C.gen_random () in
+    let s = insert elt2 (singleton elt1) in
+    let new_s = fold (fun elmt d -> insert elt d) empty s in
+    assert(member new_s elt);
+    let newer_s = remove elt new_s in
+    assert(is_empty newer_s);
+    ()
+
+  (* Test fold on empty set w/ fxn to duplicate set *)
+  let test_fold_2 () =
+    let s = empty in
+    let new_s = fold (fun elt d -> insert elt d) empty s in
+    assert(is_empty new_s);
+    ()
+
+  (* Test string_of_set on regular set *)
+(*   let test_string_of_set () =
+    let s = insert (C.gen_random ()) (singleton (C.gen_random ())) in
+    let str = string_of_set s in
+    assert(str = "")
+    () *)
+
   (* add your test functions to run_tests *)
+  (* Order of tests ensure that later tests which use earlier fxns work *)
   let run_tests () =
+    test_empty () ;
+    test_is_empty () ;
+    test_is_empty_false () ;
     test_insert () ;
     test_insert_2 () ;
+    test_member_true () ;
+    test_member_false () ;
+    test_remove_1 () ;
+    test_remove_2 () ;
+    test_remove_3 () ;
+    test_singleton_elt () ;
+    test_singleton_size_1 () ;
+    test_union_1 () ;
+    test_union_2 () ;
+    test_union_3 () ;
+    test_union_4 () ;
+    test_intersect_1 () ;
+    test_intersect_2 () ;
+    test_intersect_3 () ;
+    test_intersect_4 () ;
+    test_choose_1 () ;
+    test_choose_2 () ;
+    test_choose_3 () ;
+    test_fold_1 () ;
+    test_fold_2 () ;
     ()
 end
 
@@ -346,10 +595,10 @@ let _ = IntListSet.run_tests()
  *
  * Uncomment out the lines below when you are ready to test your
  * 2-3 dict set implementation *)
-(*
+
 module IntDictSet = DictSet(IntComparable)
 let _ = IntDictSet.run_tests()
-*)
+
 
 
 (******************************************************************)
@@ -359,6 +608,6 @@ let _ = IntDictSet.run_tests()
 module Make(C : COMPARABLE) : (SET with type elt = C.t) =
   (* Change this line to use our dictionary implementation when your are
    * finished. *)
-  ListSet (C)
-  (* DictSet (C) *)
+  (* ListSet (C) *)
+  DictSet (C)
 
